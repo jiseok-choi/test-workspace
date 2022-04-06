1c23b3e8ed62cf8677a03e332178b76093b5865a
jiseok-choi <choi300300@gmail.com>
2022-04-07 01:11:28 +0900

    Apply .gitignore

--- mybatis/build/resources/main/mybatis/initdata/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO award (name, year) values ('칸', 2022);
-INSERT INTO award (name, year) values ('어워드', 2023);
-INSERT INTO award (name, year) values ('영화제', 2023);
-INSERT INTO award (name, year) values ('파이널', 2024);
